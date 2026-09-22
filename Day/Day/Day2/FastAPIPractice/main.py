455a303ca5fd59a1d6aeaa94891de5085852ae5b
harsha123818 <harshaakul310@gmail.com>
2026-09-22 13:20:40 +0530

    Path parameter

--- Day/Day/Day2/FastAPIPractice/main.py
+++ Day/Day/Day2/FastAPIPractice/main.py
@@ -15,3 +15,8 @@ def health():
 @app.post("/create")
 def create_something():
     return {"message":"Created"}
+
+#Path parameter
+@app.get("/student/{usn}")
+def get_result(usn):
+    return {"Result":"Distinction","usn":usn}
